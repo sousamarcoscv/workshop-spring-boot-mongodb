052ab6d191b4a3b5f2638c8705a3f69d80242578
Marcos Sousa <sousa.marcoscv@gmail.com>
2021-06-03 11:44:47 -0300

    Inserção de Usuario com Post

--- src/main/java/com/marcossousa/workshopmongo/services/UserService.java
+++ src/main/java/com/marcossousa/workshopmongo/services/UserService.java
@@ -7,6 +7,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.marcossousa.workshopmongo.domain.User;
+import com.marcossousa.workshopmongo.dto.UserDTO;
 import com.marcossousa.workshopmongo.repository.UserRepository;
 import com.marcossousa.workshopmongo.services.exception.ObjectNotFoundException;
 
@@ -25,5 +26,14 @@ public class UserService {
 		return obj.orElseThrow(() -> new ObjectNotFoundException("Objeto Não Encontrado"));
 	
 	}
+	
+	public User insert(User obj) {
+		return repo.insert(obj);
+	}
+	
+	public User fromDTO(UserDTO objDto) {
+		return new User(objDto.getId(),objDto.getName(),objDto.getEmail());
+	}
+
 
 }
