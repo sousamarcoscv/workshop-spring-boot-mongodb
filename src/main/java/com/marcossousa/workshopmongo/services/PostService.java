d9ea64422d795328e8a5e583d5aa8e4e96361975
Marcos Sousa <sousa.marcoscv@gmail.com>
2021-06-03 15:10:54 -0300

    Obtendo Post por Id

---
+++ src/main/java/com/marcossousa/workshopmongo/services/PostService.java
@@ -0,0 +1,32 @@
+package com.marcossousa.workshopmongo.services;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.marcossousa.workshopmongo.domain.Post;
+import com.marcossousa.workshopmongo.repository.PostRepository;
+import com.marcossousa.workshopmongo.services.exception.ObjectNotFoundException;
+
+@Service
+public class PostService {
+
+	@Autowired
+	private PostRepository repo;
+
+	public List<Post> findAll() {
+		return repo.findAll();
+	}
+
+	public Post findById(String id) {
+		Optional<Post> obj = repo.findById(id);
+		return obj.orElseThrow(() -> new ObjectNotFoundException("Objeto Não Encontrado"));
+	
+	}
+	
+	
+
+
+}
