38d49a03cf154c9f27a0230acf335dacbf779e58
Marcos Sousa <sousa.marcoscv@gmail.com>
2021-06-03 20:50:40 -0300

    Consulta com Varios criterios.

--- src/main/java/com/marcossousa/workshopmongo/resources/util/URL.java
+++ src/main/java/com/marcossousa/workshopmongo/resources/util/URL.java
@@ -2,6 +2,10 @@ package com.marcossousa.workshopmongo.resources.util;
 
 import java.io.UnsupportedEncodingException;
 import java.net.URLDecoder;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.TimeZone;
 
 public class URL {
 
@@ -14,6 +18,17 @@ public class URL {
 			e.printStackTrace();
 			return "";
 		}
+	}
+	
+	public static Date convertDate(String textDate, Date defaultValue) {
+		SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd");
+		sdf.setTimeZone(TimeZone.getTimeZone("GMT"));
+		try {
+			return sdf.parse(textDate);
+		} catch (ParseException e) {
+			return defaultValue;
+		}
+		
 	}
 	
 }
